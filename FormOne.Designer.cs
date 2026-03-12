5a02c99d00ffe32159b5d5a69dd8f6928c562e6f
zenin5479 <zenin5479@gmail.com>
2026-03-12 20:11:09 +0300

    Update 0.8 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -34,7 +34,7 @@
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         ClientSize = new System.Drawing.Size(681, 431);
+         ClientSize = new System.Drawing.Size(484, 361);
          Name = "FormOne";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
          Text = "Использование BackgroundWorker в Windows Forms";
