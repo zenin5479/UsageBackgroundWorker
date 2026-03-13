290dab465eb85975226271c244ad42517287e48e
zenin5479 <zenin5479@gmail.com>
2026-03-13 10:34:08 +0300

    Update 1.3 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -29,6 +29,7 @@
       private void InitializeComponent()
       {
          startButton = new System.Windows.Forms.Button();
+         button1 = new System.Windows.Forms.Button();
          SuspendLayout();
          // 
          // startButton
@@ -40,11 +41,21 @@
          startButton.Text = "Запустить расчёты";
          startButton.UseVisualStyleBackColor = true;
          // 
+         // button1
+         // 
+         button1.Location = new System.Drawing.Point(80, 157);
+         button1.Name = "button1";
+         button1.Size = new System.Drawing.Size(75, 23);
+         button1.TabIndex = 1;
+         button1.Text = "button1";
+         button1.UseVisualStyleBackColor = true;
+         // 
          // FormOne
          // 
          AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
          AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
          ClientSize = new System.Drawing.Size(484, 361);
+         Controls.Add(button1);
          Controls.Add(startButton);
          Name = "FormOne";
          StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -55,5 +66,6 @@
       #endregion
 
       private System.Windows.Forms.Button startButton;
+      private System.Windows.Forms.Button button1;
    }
 }
