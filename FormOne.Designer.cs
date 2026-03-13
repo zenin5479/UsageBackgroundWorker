6314882555a7c61c5c36b13c7e49fc835c75c118
zenin5479 <zenin5479@gmail.com>
2026-03-13 10:34:27 +0300

    Update 1.4 Обновлен метод. Проведен рефакторинг

--- FormOne.Designer.cs
+++ FormOne.Designer.cs
@@ -45,7 +45,7 @@
          // 
          button1.Location = new System.Drawing.Point(80, 157);
          button1.Name = "button1";
-         button1.Size = new System.Drawing.Size(75, 23);
+         button1.Size = new System.Drawing.Size(114, 23);
          button1.TabIndex = 1;
          button1.Text = "button1";
          button1.UseVisualStyleBackColor = true;
